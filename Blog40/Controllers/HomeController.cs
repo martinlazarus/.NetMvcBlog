71d62400636a9311eb2f970db57422637059debe
Sidney Andrews <sidney@seesharprun.net>
2017-03-30 15:52:48 -0400

    Finished Project

--- Blog40/Controllers/HomeController.cs
+++ Blog40/Controllers/HomeController.cs
@@ -1,20 +1,58 @@
-﻿using System;
+﻿using AutoMapper;
+using Blog40.Models;
+using Blog40.Repository;
+using Blog40.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Net;
 using System.Web.Mvc;
 
 namespace Blog40.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
+        private IRepository<Post> _postRepository;
+        private IMapper _mapper;
 
-        public ActionResult Index()
+        public HomeController(IRepository<Post> postRepository, IMapper mapper)
         {
-            return View();
+            _postRepository = postRepository;
+            _mapper = mapper;
         }
 
+        [HttpGet]
+        public ActionResult Index()
+        {
+            IEnumerable<Post> postList = _postRepository.GetAll().Take(5);
+            IEnumerable<PostViewModel> posts = _mapper.Map<IEnumerable<Post>, IEnumerable<PostViewModel>>(postList);
+            PostListViewModel viewModel = new PostListViewModel
+            {
+                Posts = posts
+            };
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public ActionResult Post(string slug)
+        {
+            if (String.IsNullOrEmpty(slug))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            else
+            {
+                Post match = _postRepository.GetByString(slug);
+                PostViewModel viewModel = _mapper.Map<Post, PostViewModel>(match);
+                if (match == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    return View(viewModel);
+                }
+            }
+        }
     }
 }
